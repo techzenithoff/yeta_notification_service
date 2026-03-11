b3267581523cc16b54c088e0f206270a9a953888
thkernel <salut.amos@gmail.com>
2026-03-11 10:37:45 +0000

    update

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,13 +3,13 @@ class NotificationMailer < ApplicationMailer
   #default from: 'noreply@tonapp.com'
 
     default from: "YETA+ <#{ENV["#{Rails.env.upcase}_EMAIL_USERNAME"]}>"
-  # 🔹 OTP dynamique
+  # OTP dynamique
   def send_otp(recipient, subject)
     @data = params[:data]
     mail(to: recipient, subject: subject)
   end
 
-  # 🔹 Email générique fallback
+  # Email générique fallback
   def generic_email(recipient, subject, message)
     @message = message
     mail(to: recipient, subject: subject)
